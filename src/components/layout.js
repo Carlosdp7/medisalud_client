fecc561ca4fc8c4d458c81a53b0194410f7c89a2
Carlosdp7 <danielperandrad10@gmail.com>
2022-02-08 13:34:29 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,15 +23,6 @@ const Layout = ({ children }) => {
         `}
       />
       <Helmet>
-        <link rel="stylesheet" href="@sweetalert2/theme-bootstrap-4/bootstrap-4.css" />
-        <script src="sweetalert2/dist/sweetalert2.min.js"></script>
-        <link
-          rel="preload"
-          as="style"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
-        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
