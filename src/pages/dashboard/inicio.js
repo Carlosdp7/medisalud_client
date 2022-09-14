1689f41fc14f757beebfa18c9f9960403a9729e4
Carlosdp7 <danielperandrad10@gmail.com>
2022-09-14 09:44:04 -0400

    Update inicio.js

--- src/pages/dashboard/inicio.js
+++ src/pages/dashboard/inicio.js
@@ -137,7 +137,7 @@ const Home = () => {
                       <td>{test.di}</td>
                       <td>
                         <Moment format="DD/MM/YYYY">
-                          {test.createdAt}
+                          {test.admissionDate ?? test.createdAt}
                         </Moment>
                       </td>
                       <td>{convertMilitaryToStandardTime(test.time)}</td>
